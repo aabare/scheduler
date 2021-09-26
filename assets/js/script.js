7dc109cb956688214cc60cccee033236ff276c75
Adam <a.6942@yahoo.com>
2021-09-26 19:30:18 -0400

    working on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,30 @@
 $(document).ready(function () {
-    // console.log("Ready to go!"); code works!
   
-    $("#todaysDate").text(moment().format("dddd, MMMM Do YYYY")); //displays the date format from moment for the id todaysDate
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); 
   
-    function whatTime() { //create a function to determine what time it is for planner blocks
-      var currentTime = moment().hours(); //establish the current time
+    function Time() { 
+      var present = moment().hours();
   
-      $(".time-slot").each(function () {
-        var timeSlot = parseInt($(this).attr("id").split("-")[1]); //each time slot returns as an integer
+      $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("-")[1]); 
   
-        //using an else if statement to determing past, present or future for schedule blocks
-        if (timeSlot < currentTime) { // if hour is greater than current time, add the class past
+        
+        if (timeBlock < present) { 
           $(this).addClass("past");
         }
-        else if (timeSlot === currentTime) { //if hour is the same as the current hour
-          $(this).removeClass("past"); // past class will be removed
-          $(this).addClass("present"); // present class will be added 
+        else if (timeSlot === present) { 
+          $(this).removeClass("past"); 
+          $(this).addClass("present"); 
         }
         else {
-          $(this).removeClass("past");  //else past class will be removed
-          $(this).removeClass("present"); // else present class will be removed
-          $(this).addClass("future"); // future class will be added 
+          $(this).removeClass("past");  
+          $(this).removeClass("present"); 
+          $(this).addClass("future"); 
         }
       });
     }
   
-    whatTime(); //run the function 
-  
-    // any inputted data will be saved in the local storage 
+    Time();  
     $("#hour-9").children(".description").val(localStorage.getItem("hour-9"));
     $("#hour-10").children(".description").val(localStorage.getItem("hour-10"));
     $("#hour-11").children(".description").val(localStorage.getItem("hour-11"));
@@ -38,7 +35,7 @@ $(document).ready(function () {
     $("#hour-16").children(".description").val(localStorage.getItem("hour-16"));
     $("#hour-17").children(".description").val(localStorage.getItem("hour-17"));
   
-    $(".saveBtn").on("click", function () { //click functions for saving data
+    $(".saveBtn").on("click", function () { 
       var plannerItem = $(this).siblings(".description").val();
       console.log(plannerItem);
       var listItem = $(this).parent().attr("id");
