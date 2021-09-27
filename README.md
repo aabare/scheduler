56d0d0a59ed5dfcf7ce7f2c4e5968338aa1b671f
Adam <a.6942@yahoo.com>
2021-09-26 20:21:25 -0400

    added README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work Day Scheduler Starter Code
+Work Day Scheduler 
+Introduction
+
+The Work Day Scheduler was created for Challenge 5 and includes JavaScript, HTML, Bootstrap, Third Party APIs and CSS files. The Work Day Scheduler includes time blocks from 9am- 5pm in which you can add upcoming events for the current work day. Time blocks in the present will be colored red, time blocks in the past will be colored gray, and time blocks in the future will be colored green. The user can save events within each time block.
+
+Usage 
+
+Once on the Work Day Scheduler the user clicks in each individual time block, types events happening during each time during the day and then saves events during the work day(9am-5pm). As the work day progresses the time blocks in the past change to gray. If the event is during a present time the time block will be red indicating that it is a currently happening event. Future events will be colored green to show that they are going to occur. 
+
+Authors and Acknowledgements
+
+This code was prepared for Challenge 5 for Rutgers University Bootcamp by Adam Abare
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,50 @@
 $(document).ready(function () {
 
-    //Moment.JS
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); 
+//Moment.JS
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); 
 
-
-    //Changes class based on present time 
-    function Time() { 
-      var presentTime = moment().hours();
+//Changes class based on present time 
+function Time() { 
+  var presentTime = moment().hours();
   
-      $(".time-block").each(function () {
-        var timeBlock = parseInt($(this).attr("id").split("-")[1]); 
+$(".time-block").each(function () {
+  var timeBlock = parseInt($(this).attr("id").split("-")[1]); 
   
-        //Adds past class
-        if (timeBlock < presentTime) { 
-          $(this).addClass("past");
-        }
-        //Adds present class
-        else if (timeBlock === presentTime) { 
-          $(this).removeClass("past"); 
-          $(this).addClass("present"); 
-        }
-        //Adds future class
-        else {
-          $(this).removeClass("past");  
-          $(this).removeClass("present"); 
-          $(this).addClass("future"); 
-        }
-      });
-    }
+//Adds past class
+  if (timeBlock < presentTime) { 
+    $(this).addClass("past"); 
+ 
+ //Adds present class
+  }else if (timeBlock === presentTime) { 
+      $(this).removeClass("past"); 
+      $(this).addClass("present"); 
+   
+   //Adds future class
+    }else{
+      $(this).removeClass("past");  
+      $(this).removeClass("present"); 
+      $(this).addClass("future"); 
+  
+    }});
     
-    //Time Function
-    Time(); 
-    $("#hour-9").children(".description").val(localStorage.getItem("hour-9"));
-    $("#hour-10").children(".description").val(localStorage.getItem("hour-10"));
-    $("#hour-11").children(".description").val(localStorage.getItem("hour-11"));
-    $("#hour-12").children(".description").val(localStorage.getItem("hour-12"));
-    $("#hour-13").children(".description").val(localStorage.getItem("hour-13"));
-    $("#hour-14").children(".description").val(localStorage.getItem("hour-14"));
-    $("#hour-15").children(".description").val(localStorage.getItem("hour-15"));
-    $("#hour-16").children(".description").val(localStorage.getItem("hour-16"));
-    $("#hour-17").children(".description").val(localStorage.getItem("hour-17"));
+  }
+    
+//Time Function
+ Time(); 
+  $("#hour-9").children(".description").val(localStorage.getItem("hour-9"));
+  $("#hour-10").children(".description").val(localStorage.getItem("hour-10"));
+  $("#hour-11").children(".description").val(localStorage.getItem("hour-11"));
+  $("#hour-12").children(".description").val(localStorage.getItem("hour-12"));
+  $("#hour-13").children(".description").val(localStorage.getItem("hour-13"));
+  $("#hour-14").children(".description").val(localStorage.getItem("hour-14"));    $("#hour-15").children(".description").val(localStorage.getItem("hour-15"));
+  $("#hour-16").children(".description").val(localStorage.getItem("hour-16"));
+  $("#hour-17").children(".description").val(localStorage.getItem("hour-17"));
   
-
-    //Local Storage
-    $(".saveBtn").on("click", function () { 
-      var todo = $(this).siblings(".description").val();
-      var savedItem = $(this).parent().attr("id");
-      localStorage.setItem(savedItem, JSON.stringify(todo));
-    });
-  
+ //Local Storage
+  $(".saveBtn").on("click", function () { 
+    var todo = $(this).siblings(".description").val();
+    var savedItem = $(this).parent().attr("id");
+    localStorage.setItem(savedItem, JSON.stringify(todo));
+   
+  });
   });
